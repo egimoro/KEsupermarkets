1db602fa876b23b4436a6c48999bfceb6d3ae07c
egimoro <gimoremma@gmail.com>
2020-02-08 21:49:38 +0000

    updated jupyter notebook for visual studio and python 3.8.1

--- KEsupermarket.py
+++ KEsupermarket.py
@@ -1,6 +1,7 @@
 # %%
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 supermarkets = ['acacia', 'acacia', 'acacia', 'acacia', 'nakumatt', 'tuskys',
@@ -21,15 +22,40 @@ KEsupermarket = list(zip(supermarkets, no_of_items, variation, total))
 
 print(KEsupermarket)
 
+# %%
+
 df = pd.DataFrame(data=KEsupermarket, columns=['supermarkets', 'no_of_items',
                   'variation', 'total'])
 
 print(df)
 
+# %%
+
 Sorted = df.sort_values(['supermarkets'], ascending=False)
 
 Sorted.head(1)
 
-df['no_of_items'].max()
+# %%
+
+print(df['no_of_items'].max())
+
+# %%
+
+df['variation'].plot()
+
+MaxValue = df['variation'].max()
+
+MaxSupermarket = df['supermarkets'][df['variation'] == df['variation'
+                    ].max()].values
+
+Text = str(MaxValue) + " - " + MaxSupermarket
+
+plt.annotate(Text, xy=(1, MaxValue), xytext=(8, 0), 
+             xycoords=('axes fraction', 'data'), textcoords='offset points')
+
+print("The most popular supermarket")
+
+df[df['supermarkets'] == df['supermarkets'].max()]
+
 
 # %%

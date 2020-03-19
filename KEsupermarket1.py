555d05b8ae8a59e1fc268fa06c944bf44968a2ee
egimoro <gimoremma@gmail.com>
2020-03-19 01:16:23 +0000

    Read full dataset of Kenyan Supermarkets

--- KEsupermarket1.py
+++ KEsupermarket1.py
@@ -1,7 +1,61 @@
 # %%
 
 import pandas as pd
-import numpy as np 
+import numpy as np
 
-# Create lists
+# Create a data frame of supermarkets
 
+df = pd.DataFrame({'Supermarket': np.random.choice(['acacia', 'chandarana',
+                  'tuskys', 'nakumatt', 'uchumi', 'eastmatt', 'ukwala',
+                   'fairway', 'tumaini'], size=1000),
+                   'Total paid': np.random.randint(70, 13000, size=1000),
+                   'type': np.random.choice(['cash', 'card', 'mpesa'],
+                                            size=1000)})
+
+df                             
+
+# %%
+
+# Number of observations in the dataset
+
+df.shape[0]
+
+# %%
+
+df.info()
+
+# %%
+
+# See first 10 entries
+
+df.head(10)
+
+# %%
+
+# Number of columns in the dataset
+
+df.shape[1]
+
+# %%
+
+# Print the name of all the columns
+
+df.columns
+
+# %%
+
+# How is the dataset indexed?
+
+df.index
+
+# %%
+
+# Read full dataset
+
+Location = r'../KEsupermarkets/data.csv'
+
+df = pd.read_csv(Location)
+
+df
+
+# %%

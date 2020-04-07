3875ed57419bb06084d902fac6d86028c3bc194b
egimoro <gimoremma@gmail.com>
2020-04-08 00:41:39 +0100

    filtering and sorting

--- KEsupermarket1.py
+++ KEsupermarket1.py
@@ -1,4 +1,4 @@
-# %%
+# %% 
 
 import pandas as pd
 import numpy as np
@@ -15,7 +15,7 @@ df = pd.DataFrame({'Supermarket': np.random.choice(['acacia', 'chandarana',
                    'type': np.random.choice(['cash', 'card', 'mpesa'],
                                             size=1000)})
 
-df                  
+df                 
 
 # %%
 
@@ -140,3 +140,150 @@ unitPrice
 df['unitPrice'] = np.round(unitPrice, 2)
 
 df
+
+# %%
+
+# Group unitPrice by intervals of 100 for prices [0,5000),
+#  and sum no_of_items and total
+
+price_start = 0
+
+price_end = 5000
+
+price_interval = 100
+
+# Creating the buckets to collect the data accordingly
+
+buckets = np.arange(price_start, price_end, price_interval) 
+
+# Select the data and sum
+
+revenue_per_price = df.groupby(pd.cut(df.unitPrice,
+                               buckets)).total.sum()
+
+revenue_per_price.head()
+
+# %%
+
+revenue_per_price.plot()
+
+plt.xlabel('Unit Price (in intervals of '+str(price_interval)+')')
+
+plt.ylabel('Total')
+
+plt.show()
+
+# %%
+
+# Count how many items are food
+
+food_item = df[df.food == 'yes']
+
+len(food_item)
+
+# %%
+
+# Set the 'type_market' column as the index of the dataframe
+
+df.set_index('type_market', inplace=True)
+
+# Print only the beverage column
+
+df.beverage
+
+# %%
+
+# Print the columns 'mall' and 'bevarage'
+
+df[['mall', 'beverage']]
+
+# %%
+
+# Name of all columns
+
+df.columns
+
+# %%
+
+# Select the 'Supermarket', 'Change', 'Type' columns from small and chain
+
+df.loc[['small', 'chain'], ['supermarket', 'change', 'type']]
+
+# %%
+
+# Select the rows 3 to 7 and the columns 3 to 6
+
+df.iloc[2:7, 2:6]
+
+# %%
+
+# Select every row after the fourth and all columns
+
+df.iloc[4:, :]
+
+# %%
+
+# Select every row up to the 4th row and all columns
+
+df.iloc[:4, :]
+
+# %%
+
+# Select the 3rd column up to the 7th column
+
+df.iloc[:, 2:7]
+
+# %%
+
+# Select rows where df.change is greater than 1500
+
+df[df['change'] > 1500]
+
+# %%
+
+# Select rows where df.change is greater than 1500 or less than 150
+
+df[(df['change'] > 1500) | (df['change'] < 150)]
+
+# %%
+
+# Select all the location not 'saika'
+
+df[df['location'] != 'saika']
+
+# %%
+
+# Reset index
+
+df.reset_index()
+
+# %%
+
+# Set column 'time_type' as index
+
+try:
+    df.set_index('time_type', inplace=True)
+except Exception:
+    print('Something wrong')
+
+df
+
+# %%
+
+# Select rows morning and evening
+
+df.loc[['morning', 'evening']]
+
+# %%
+
+# Select the third cell in the row afternoon
+
+df.loc[['afternoon']].iloc[:, 2]
+
+# %%
+
+# Select the third cell down in the column named variation
+
+df.loc[:, ['variation']].iloc[2]
+
+# %%
